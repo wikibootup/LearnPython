fbe0dd0b9114324c8f05ca2465a50f8ec769a672
wikibootup <wikibootup@JWui-MacBook-Pro.local>
2014-07-26 06:34:01 +0900

    LearnPython12

--- ex12.py
+++ ex12.py
@@ -17,6 +17,6 @@ height = raw_input()
 print "How much do you weigh?",
 weight = raw_input()
 
-print "So, you're %s old, %r tall and %r heavy." % (
+print "So, you're %r old, %r tall and %r heavy." % (
 	age, height, weight)
 
