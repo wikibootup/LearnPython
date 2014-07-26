191cc90e49605329aa0e9eb0c8001f896d63417d
wikibootup <wikibootup@JWui-MacBook-Pro.local>
2014-07-27 03:10:41 +0900

    LearnPython19

--- ex18.py
+++ ex18.py
@@ -1,6 +1,11 @@
 #from sys import argv
 
 # this one is like your scripts with argv
+#def print_inserted(**argv)
+#	arg1 = argv
+#	arg2 = argv
+#	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
 def print_two(*args):
     arg1, arg2 = args
     print "arg1: %r, arg2: %r" % (arg1, arg2)
@@ -16,7 +21,6 @@ def print_one(arg1):
 # this one takes no arguments
 def print_none():
     print "I got nothin'."
-
 
 print_two("Zed","Shaw")
 print_two_again("Zed","Shaw")
