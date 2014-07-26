e5365def0723c777f347e9d36310ebb4a38cc8f4
wikibootup <wikibootup@JWui-MacBook-Pro.local>
2014-07-27 04:11:42 +0900

    LearnPython21

---
+++ ex21.py
@@ -0,0 +1,61 @@
+#
+#def add(a, b):
+#    print "ADDING %d + %d" % (a, b)
+#    return a + b
+#
+#def subtract(a, b):
+#    print "SUBTRACTING %d - %d" % (a, b)
+#    return a - b
+#
+#def multiply(a, b):
+#    print "MULTIPLYING %d * %d" % (a, b)
+#    return a * b
+#
+#def divide(a, b):
+#    print "DIVIDING %d / %d" % (a, b)
+#    return a / b
+#
+#
+#print "Let's do some math with just functions!"
+#
+#age = add(30, 5)
+#height = subtract(78, 4)
+#weight = multiply(90, 2)
+#iq = divide(100, 2)
+#
+#print "Age: %d, Height: %d, Weight: %d, IQ: %d" % (age, height, weight, iq)
+#
+#
+## A puzzle for the extra credit, type it in anyway.
+#print "Here is a puzzle."
+
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
+
+print "That becomes: ", what, "Can you do it by hand?"
+
+
+def add(a, b):
+	print "ADDING %d + %d" % (a, b)
+	return a + b
+
+def subtract(a, b):
+	print "SUBTRACTING %d - %d" % (a, b)
+	return a - b
+
+def multiply(a, b):
+	print "MULTIPLYING %d * %d" % (a, b)
+	return a * b
+
+def divide(a, b):
+	print "DIVIDING %d / %d" % (a, b)
+	return a / b
+ 
+print "Let's go."
+
+age = add(30, 5)
+height = subtract(30,5)
+weight = multiply(10, 2)
+iq = divide(4, 2)
+
+print "age = %d, height =  %d, weight =  %d, iq = %d" % (age, height, weight, iq)
+
